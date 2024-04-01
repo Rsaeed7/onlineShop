76c12ec7e2bacce99440ad72144e887580b96b88
Saeed <saye00n@gmail.com>
2024-04-01 21:54:34 +0330

    create a form

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render,redirect
 from .models import ContactUs
 from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 def contactUs(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        ContactUs.objects.create(name=name, email=email, message=message)
-        messages.success(request, 'Thank you! you have successfully sent your message!')
-        return redirect('contact:contactus')
-
-    return render(request,'core/countact.html')
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            ContactUs.objects.create(name=cd['name'],email=cd['email'],message=cd['message'])
+            messages.success(request, 'Thank you! you have successfully sent your message!')
+            return redirect('contact:contactus')
+    form = ContactForm(request.POST)
+    return render(request,'core/countact.html',{'form':form})
